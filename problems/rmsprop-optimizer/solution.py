53c5937e68811e7c9171fff56a13b017089b62ad
Ishaan Shrivastava <ishaanshri95@gmail.com>
2025-11-07 13:21:45 +0530

    correct solution: rmsprop-optimizer

--- problems/rmsprop-optimizer/solution.py
+++ problems/rmsprop-optimizer/solution.py
@@ -6,5 +6,5 @@ def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
     """
     w, g, s = list(map(lambda x: np.array(x).astype(np.longdouble), [w, g, s]))
     s = beta * s + (1 - beta) * g * g
-    w = w - (lr) * (g / np.sqrt(s + eps))
+    w = w - (lr) * (g / (np.sqrt(s) + eps)
     return w, s
